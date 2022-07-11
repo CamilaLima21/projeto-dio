2e8dc26fc2a5cc47af06a500e393eca2f81456e1
Camila Marques de Lima <95050431+CamilaLima21@users.noreply.github.com>
2022-07-10 21:04:51 -0300

    Testando a sobrecarga de métodos

---
+++ projeto-dio/aula2/TestaSobrecarga.java
@@ -0,0 +1,17 @@
+package br.com.dio.aula2;
+
+public class TestaSobrecarga {
+
+	public static void main(String[] args) {
+		
+		/// Quadrilatero
+		System.out.println("Exercício Quadrilátero");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5d, 5d);
+		Quadrilatero.area(7, 8, 9);
+		Quadrilatero.area(5f, 5f);
+		System.out.println("=================================================");
+
+	}
+
+}
